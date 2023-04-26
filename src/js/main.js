968cabb06818a321cfe15a4ca20873f1ae67a216
Dusan Brankov <dusanbrankov@proton.me>
2023-04-26 22:47:49 +0200

    Normalize position of 'hero' image

--- src/js/main.js
+++ src/js/main.js
@@ -6,14 +6,14 @@ const menuButtonLabel = document.querySelector('.main-nav__menu-toggle-label');
 const navItems = document.querySelectorAll('.main-nav__item');
 
 menuButton.addEventListener('click', () => {
-    body.classList.toggle('is-active');
-    if (body.classList.contains('is-active')) {
-        this.setAttribute('aria-expanded', 'false');
-        menuButtonLabel.innerHTML = 'Open main menu';
-    } else {
-        this.setAttribute('aria-expanded', 'true');
-        menuButtonLabel.innerHTML = 'Close main menu';
-    }
+  body.classList.toggle('is-active');
+  if (body.classList.contains('is-active')) {
+    this.setAttribute('aria-expanded', 'false');
+    menuButtonLabel.innerHTML = 'Open main menu';
+  } else {
+    this.setAttribute('aria-expanded', 'true');
+    menuButtonLabel.innerHTML = 'Close main menu';
+  }
 });
 
 // Toggle mobile submenu
@@ -21,9 +21,9 @@ menuButton.addEventListener('click', () => {
 const submenuButtons = document.querySelectorAll('.main-nav__submenu-toggle');
 
 submenuButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        button.classList.toggle('main-nav__submenu-toggle--is-active');
-    });
+  button.addEventListener('click', () => {
+    button.classList.toggle('main-nav__submenu-toggle--is-active');
+  });
 });
 
 // Get height of site header
