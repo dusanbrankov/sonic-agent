e9f59863bda2595aa77fd87e9d5f67c4697b8be2
Dusan Brankov <dusanbrankov@proton.me>
2023-05-02 19:24:54 +0200

    Exclude directory "sass" from "eleventyConfig"

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,11 @@
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
 module.exports = function (eleventyConfig) {
-  // eleventyConfig.addWatchTarget("./src/assets/scss/");
+  eleventyConfig.addWatchTarget("./src/assets/sass/");
   eleventyConfig.addWatchTarget("./src/assets/js/");
-  eleventyConfig.addPassthroughCopy("./src/assets/");
+  eleventyConfig.addPassthroughCopy("./src/assets/fonts");
+  eleventyConfig.addPassthroughCopy("./src/assets/img");
+  eleventyConfig.addPassthroughCopy("./src/assets/js");
 
   eleventyConfig.setBrowserSyncConfig({
     port: 3000
